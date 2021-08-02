8b3d377dcc25bbd0ed5b904de2c79474890b1744
Kim-Gyuri <gyuring0203@gmail.com>
2021-08-02 17:31:18 +0900

    commit done!

--- src/test/java/Hello/core/lifecycle/NetworkClient.java
+++ src/test/java/Hello/core/lifecycle/NetworkClient.java
@@ -29,15 +29,17 @@ public class NetworkClient  {
         System.out.println("close=" + url);
     }
 
+
     @PostConstruct
     public void init() {
-        System.out.println("NetworkClient.init()");
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
+
     @PreDestroy
     public void close() {
-        System.out.println("NetworkClient.close()");
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
